c59292cdc547620104d10acbb272ac2dc95f7341
Ladislav Petera <ladislav.petera@yahoo.de>
2018-02-28 23:48:43 +0100

    formatted

--- setup-swarm.sh
+++ setup-swarm.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+# ---------------------------------------------------------
+# prerequisites - initialized swarm
+# ---------------------------------------------------------
+
 # config
 export REGISTRY_PORT=5000
 export REGISTRY=localhost:$REGISTRY_PORT
-
-# init swarm
-docker swarm init
 
 # setup registry service
 docker service create --replicas 1 --name registry -p $REGISTRY_PORT:5000 registry:latest
@@ -13,19 +14,19 @@ docker service create --replicas 1 --name registry -p $REGISTRY_PORT:5000 regist
 docker build -t $REGISTRY/peteral/nodered:latest ./nodered
 
 docker pull eclipse-mosquitto
-docker tag eclipse-mosquitto $REGISTRY/eclipse-mosquitto
+docker tag  eclipse-mosquitto $REGISTRY/eclipse-mosquitto
 docker push $REGISTRY/eclipse-mosquitto
 
 docker pull influxdb
-docker tag influxdb $REGISTRY/influxdb
+docker tag  influxdb $REGISTRY/influxdb
 docker push $REGISTRY/influxdb
 
 docker pull grafana/grafana
-docker tag grafana/grafana $REGISTRY/grafana/grafana
+docker tag  grafana/grafana $REGISTRY/grafana/grafana
 docker push $REGISTRY/grafana/grafana
 
 # create services
-docker service create --replicas 1 --name nodered_nodered_1 -p 80:1880 $REGISTRY/peteral/nodered
-docker service create --replicas 1 --name nodered_mosquitto_1 -p 1883:1883 -p 9001:9001 $REGISTRY/eclipse-mosquitto
-docker service create --replicas 1 --name nodered_influxfb_1 -p 8086:8086 $REGISTRY/influxdb
-docker service create --replicas 1 --name nodered_grafana_1 -p 3000:3000 $REGISTRY/grafana/grafana
+docker service create --replicas 1 --name nodered   -p 80:1880                  $REGISTRY/peteral/nodered
+docker service create --replicas 1 --name mosquitto -p 1883:1883 -p 9001:9001   $REGISTRY/eclipse-mosquitto
+docker service create --replicas 1 --name influxdb  -p 8086:8086                $REGISTRY/influxdb
+docker service create --replicas 1 --name grafana   -p 3000:3000                $REGISTRY/grafana/grafana

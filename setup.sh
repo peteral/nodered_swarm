46a24a05906cea69c58b3a686aa0971897471ea0
peteral <ladislav.petera@eisenmann.com>
2018-03-02 13:47:53 +0100

    added logging

--- setup.sh
+++ setup.sh
@@ -24,46 +24,53 @@
 export REGISTRY_PORT=5000
 export REGISTRY=localhost:$REGISTRY_PORT
 
-# create isolated overlay network for our services
+echo "---> create network"
 docker network create --driver overlay nodered
 
-# setup registry service
+echo "---> set up swarm registry"
 docker service create --network nodered --replicas 1 --name registry -p $REGISTRY_PORT:5000 registry:latest
 
 # push used images to swarm registry
+echo "---> deploy nodered image"
 docker build -t $REGISTRY/peteral/nodered ./nodered
 docker push $REGISTRY/peteral/nodered
 
+echo "---> deploy mosquitto image"
 docker pull eclipse-mosquitto
 docker tag  eclipse-mosquitto $REGISTRY/eclipse-mosquitto
 docker push $REGISTRY/eclipse-mosquitto
 
+echo "---> deploy influxdb image"
 docker pull influxdb
 docker tag  influxdb $REGISTRY/influxdb
 docker push $REGISTRY/influxdb
 
+echo "---> deploy grfana image"
 docker pull grafana/grafana
 docker tag  grafana/grafana $REGISTRY/grafana/grafana
 docker push $REGISTRY/grafana/grafana
 
-# create services
+echo "---> create nodered service"
 docker service create --network nodered --replicas 1 --name nodered   -p 80:1880                  $REGISTRY/peteral/nodered
+echo "---> create mosquittod service"
 docker service create --network nodered --replicas 1 --name mosquitto -p 1883:1883 -p 9001:9001   $REGISTRY/eclipse-mosquitto
+echo "---> create influxdb service"
 docker service create --network nodered --replicas 1 --name influxdb  -p 8086:8086                $REGISTRY/influxdb
+echo "---> create grafana service"
 docker service create --network nodered --replicas 1 --name grafana   -p 3000:3000                $REGISTRY/grafana/grafana
 
-# create influx database testdb
+echo "---> create database"
 curl -X POST 'http://localhost:8086/query?pretty=true' --data-urlencode "q=create database testdb"
 
-# import grafana data source
+echo "---> create grafana data source"
 export datasource=`cat grafana/datasource.json`
 curl -X POST 'http://admin:admin@localhost:3000/api/datasources' -H "Content-Type: application/json" -d "$datasource"
 
-# import grafana dashboard
+echo "---> create grafana dashboard"
 export dashboard=`cat grafana/dashboard.json`
 export request="{ \"dashboard\": $dashboard, \"overwrite\" : true } "
 curl -X POST 'http://admin:admin@localhost:3000/api/dashboards/db' -H "Content-Type: application/json" -d "$request"
 
-# import nodered flows
+echo "---> import nodered flows"
 export flows=`cat nodered/flows.json`
 curl -X POST 'http://localhost/flows' -H "Content-Type: application/json" -d "$flows"

5f14b2a8ab8bafe0bfc9a603c6d0731572119224
peteral <ladislav.petera@eisenmann.com>
2018-03-02 12:08:38 +0100

    include config in nodered container

--- setup-swarm.sh
+++ setup-swarm.sh
@@ -3,13 +3,30 @@
 # prerequisites - initialized swarm
 # docker swarm init
 # ---------------------------------------------------------
+# Network configuration:
+#
+# -------------------------------+------------------------ host
+#                                |
+#                       ---------+---------
+#                       | docker_gwbridge |
+#                       ---------+---------
+#                                |
+# -----+---------------+---------+----+-------------+----- nodered
+#      | 80            | 1883, 9001   | 8086        | 3000
+# -----+-----   -------+-----   ------+-----   -----+-----
+# | nodered |   | mosquitto |   | influxdb |   | grafana |
+# -----------   -------------   ------------   -----------
+#
+# ---------------------------------------------------------
 
 # config
 export REGISTRY_PORT=5000
 export REGISTRY=localhost:$REGISTRY_PORT
 
+docker network create --driver overlay nodered
+
 # setup registry service
-docker service create --replicas 1 --name registry -p $REGISTRY_PORT:5000 registry:latest
+docker service create --network nodered --replicas 1 --name registry -p $REGISTRY_PORT:5000 registry:latest
 
 # push used images to swarm registry
 docker build -t $REGISTRY/peteral/nodered ./nodered
@@ -28,10 +45,10 @@ docker tag  grafana/grafana $REGISTRY/grafana/grafana
 docker push $REGISTRY/grafana/grafana
 
 # create services
-docker service create --replicas 1 --name nodered   -p 80:1880                  $REGISTRY/peteral/nodered
-docker service create --replicas 1 --name mosquitto -p 1883:1883 -p 9001:9001   $REGISTRY/eclipse-mosquitto
-docker service create --replicas 1 --name influxdb  -p 8086:8086                $REGISTRY/influxdb
-docker service create --replicas 1 --name grafana   -p 3000:3000                $REGISTRY/grafana/grafana
+docker service create --network nodered --replicas 1 --name nodered   -p 80:1880                  $REGISTRY/peteral/nodered
+docker service create --network nodered --replicas 1 --name mosquitto -p 1883:1883 -p 9001:9001   $REGISTRY/eclipse-mosquitto
+docker service create --network nodered --replicas 1 --name influxdb  -p 8086:8086                $REGISTRY/influxdb
+docker service create --network nodered --replicas 1 --name grafana   -p 3000:3000                $REGISTRY/grafana/grafana
 
 # create influx database testdb
 curl -X POST 'http://localhost:8086/query?pretty=true' --data-urlencode "q=create database testdb"
